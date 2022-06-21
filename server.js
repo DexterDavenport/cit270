6cda33de361f78a948a6a5538f3ca9a693e60db5
dexterdavenport <dexter.davenport1@gmail.com>
2022-06-21 13:43:45 -0600

    fghj

--- server.js
+++ server.js
@@ -10,10 +10,10 @@ const fs = require('fs');
 const redisClient = createClient(
 {
     url: 'redis://default@10.128.0.2:6379',
-//     socket:{
-//         port:6379,
-//         host:"127.0.0.1",
-// },
+    socket:{
+        port:6379,
+        host:"127.0.0.1",
+},
 }
 ); // This creates a connection to the redis database
 

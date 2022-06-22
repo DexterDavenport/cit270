bf23034ad64b831ecf98fb9f2bcb7d508f577d39
dexterdavenport <dexter.davenport1@gmail.com>
2022-06-22 13:26:57 -0600

    sdf

--- apiclient.sh
+++ apiclient.sh
@@ -1,2 +1,2 @@
-curl -d "@data.json" -H "content-type:application/json" -X POST https://dexter.cit270.com:443/login
+curl -d "@data.json" -H "content-type:application/json" -X POST https://dexter-nodejs.cit270.com:443/login
 # --insecre could be added be '-d'

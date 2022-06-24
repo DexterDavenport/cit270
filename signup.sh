eb896137b2e0f2e1b10319615d79503770ebacb0
dexterdavenport <dexter.davenport1@gmail.com>
2022-06-24 13:47:58 -0600

    sdf

--- signup.sh
+++ signup.sh
@@ -1,1 +1,1 @@
-curl --insecure -d "@signup.json" -H "content-type:application/json" -X POST https://dexter-nodejs.cit270.com:443/signup
+curl -d "@signup.json" -H "content-type:application/json" -X POST https://dexter-nodejs.cit270.com:443/signup

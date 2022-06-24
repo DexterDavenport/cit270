573ffc6b5ae0d0d31720d76e4a30a716b9943f9c
dexterdavenport <dexter.davenport1@gmail.com>
2022-06-24 13:16:17 -0600

    sdf

--- signup.sh
+++ signup.sh
@@ -1,1 +1,1 @@
-curl -d "@data.json" -H "content-type:application/json"  https://dexter-nodejs.cit270.com:443/signup
+curl -d "@signup.json" -X POST -H "content-type:application/json" https://dexter-nodejs.cit270.com:443/signup
